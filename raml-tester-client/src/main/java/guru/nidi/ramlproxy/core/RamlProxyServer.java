dc8a1b6a7cec8026bcebdafd6ad22b92237bd234
Stefan Niederhauser <ghuder5@gmx.ch>
2015-05-05 16:56:00 +0200

    retry stopping already running server

--- raml-tester-client/src/main/java/guru/nidi/ramlproxy/core/RamlProxyServer.java
+++ raml-tester-client/src/main/java/guru/nidi/ramlproxy/core/RamlProxyServer.java
@@ -18,11 +18,17 @@ package guru.nidi.ramlproxy.core;
 import guru.nidi.ramlproxy.report.ReportSaver;
 import guru.nidi.ramltester.RamlDefinition;
 import guru.nidi.ramltester.RamlLoaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.BindException;
 
 /**
  *
  */
 public abstract class RamlProxyServer implements AutoCloseable {
+    private static final Logger log = LoggerFactory.getLogger(RamlProxyServer.class);
+
     protected final ServerOptions options;
     private final ReportSaver saver;
     private final Thread shutdownHook;
@@ -34,11 +40,32 @@ public abstract class RamlProxyServer implements AutoCloseable {
         Runtime.getRuntime().addShutdownHook(shutdownHook);
     }
 
+    abstract protected void start() throws Exception;
+
     abstract protected boolean stop() throws Exception;
 
     abstract public void waitForServer() throws Exception;
 
     abstract public boolean isStopped();
+
+    protected void doStart() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                start();
+                return;
+            } catch (BindException e) {
+                log.warn("Another server is still running. Retry to start again in 10 ms...");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e1) {
+                    //ignore
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Problem starting server", e);
+            }
+        }
+        throw new RuntimeException("Another server is still running. Stop it.");
+    }
 
     public RamlDefinition fetchRamlDefinition() {
         return RamlLoaders.absolutely()
