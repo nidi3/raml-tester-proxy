8227de9f7d12394d4bfacd98896a5a714f5c483b
Stefan Niederhauser <ghuder5@gmx.ch>
2015-05-05 15:53:44 +0200

    use SimpleLogger

---
+++ raml-tester-client/src/main/java/guru/nidi/ramlproxy/LogConfigurer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.ramlproxy;
+
+import org.slf4j.impl.SimpleLogger;
+
+/**
+ *
+ */
+class LogConfigurer {
+    private LogConfigurer() {
+    }
+
+    public static void config() {
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "debug");
+        setLogLevel("org.eclipse.jetty", "warn");
+        setLogLevel("org.raml.parser", "warn");
+        setLogLevel("org.apache.http", "warn");
+        setLogLevel("org.apache.http", "warn");
+        setLogLevel("guru.nidi.ramlproxy.jetty.JettyProxyServlet", "info");
+    }
+
+    private static void setLogLevel(String prefix, String level) {
+        System.setProperty(SimpleLogger.LOG_KEY_PREFIX + prefix, level);
+    }
+
+}
