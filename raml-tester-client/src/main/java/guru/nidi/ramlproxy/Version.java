fbf4e37f6c16069d4dccdaa97a468feeaa6eea77
Stefan Niederhauser <ghuder5@gmx.ch>
2015-05-07 03:53:43 +0200

    update dependencies

--- raml-tester-client/src/main/java/guru/nidi/ramlproxy/Version.java
+++ raml-tester-client/src/main/java/guru/nidi/ramlproxy/Version.java
@@ -19,5 +19,5 @@ package guru.nidi.ramlproxy;
  *
  */
 public class Version {
-    public static final String VERSION = "0.0.5-SNAPSHOT";
+    public static final String VERSION = "0.8.1-SNAPSHOT";
 }
